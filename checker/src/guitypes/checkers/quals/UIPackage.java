c214e59941bdf736a258f01a43bebde4013c27b5
Timo Kinnunen <timo.kinnunen@gmail.com>
2013-09-02 18:09:24 +0300

    Reformatted.

--- checker/src/guitypes/checkers/quals/UIPackage.java
+++ checker/src/guitypes/checkers/quals/UIPackage.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Target;
 
 /**
  * Package annotation to make all classes within a package @UIType
- *
+ * 
  * Do NOT mark this @TypeQualifier. It is not necessary to get access
  * to it, and doing so directs the checker framework to add all sorts
  * of extra weird semantics to its use.
