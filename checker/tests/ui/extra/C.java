7e3c6d9d862de43ba0c1ad0a04a94229ae6f7cd1
Timo Kinnunen <timo.kinnunen@gmail.com>
2013-09-02 20:03:19 +0300

    Added extra test for Checker Framework issue.

---
+++ checker/tests/ui/extra/C.java
@@ -0,0 +1,9 @@
+// This test of unit tests only needs to be executed. Its failure or success is secondary.
+in\u0074erface I {
+    //:: error: annotation type not applicable to this kind of declaration
+    /*@checkers.quals.Bottom*/public final I i = null;
+}
+
+public clas\u0073 C {}
+
+en\u0075m E {}
